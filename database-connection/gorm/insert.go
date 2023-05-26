c10decf78c81c28c0c50e362ca9b5f97a5157334
unknown <pradasando@gmail>
2023-05-26 14:17:16 +0700

    first commit

---
+++ database-connection/gorm/insert.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	
+}
+
+func main() {
+	dsn := "host=localhost user=postgres password=Kmzway87a@ dbname=test_db_camp port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&User{})
+
+	fmt.Println("Migration Success")
+
+}
